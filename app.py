6a9ec9d228719c875929eba07eb8d37b62a41a43
shanzhisun00-max <shanzhisun00@gmail.com>
2026-08-28 17:55:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ st.markdown("""
         padding: 12px; /* 内部留白缩小 */
         box-shadow: 0 4px 20px rgba(0,0,0,0.03); 
         border: 1px solid rgba(0,0,0,0.02); 
-        margin-bottom: 24px; /* 卡片上下的间距变大 */
-        max-width: 160px; /* 强制限制卡片最大宽度，让它显得更小 */
+        margin-bottom: 20px; /* 卡片上下的间距变大 */
+        max-width: 200px; /* 强制限制卡片最大宽度，让它显得更小 */
         display: flex;
         flex-direction: column;
         justify-content: center; 
